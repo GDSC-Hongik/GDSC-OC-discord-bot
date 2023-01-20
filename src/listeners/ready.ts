482e29adcec04795ff90befd27a1c85ecf5a9bc7
developomp <developomp@gmail.com>
2023-01-20 19:02:33 +0900

    chore: remove no longer unnecessary code
    feature merged in https://github.com/sapphiredev/framework/pull/585

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -11,38 +11,6 @@ export class ReadyListener extends Listener {
 	}
 
 	public async run(client: Client) {
-		await this.deleteUnusedCommands()
-
 		this.container.logger.info(`${client?.user?.tag} 준비 완료!`)
-	}
-
-	/**
-	 * 사용되지 않는 슬래시 커맨드 자동 삭제
-	 *
-	 * https://github.com/sapphiredev/framework/pull/579
-	 */
-	private async deleteUnusedCommands() {
-		const commands = await this.container.client.application?.commands.fetch()
-
-		if (!commands) return
-
-		this.container.logger.debug(
-			`Fetched all Application Commands for the currently logged in client`
-		)
-
-		const store = this.container.stores.get("commands")
-
-		if (!store) return
-
-		const commandsToDelete = commands.filter(
-			(command) => !store.has(command.name)
-		)
-
-		for (const command of commandsToDelete.values()) {
-			this.container.logger.debug(
-				`Deleting application command ${command.name} (${command.id})`
-			)
-			await this.container.client.application?.commands.delete(command)
-		}
 	}
 }
