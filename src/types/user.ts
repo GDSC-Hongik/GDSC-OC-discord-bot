413dfefbfd038d8c176e8426b3135b611d318544
developomp <developomp@gmail.com>
2023-02-12 20:18:58 +0900

    refactor: improve "UserSchema" doc comment

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,8 +3,12 @@ import { z } from "zod"
 import { tierSchema } from "./tier"
 
 export const userSchema = z.object({
+	// user's tier based on their devRating
 	tier: tierSchema,
+
+	// total spendable points
 	points: z.number(),
+
 	// array of "YYYY/MM/DD" formatted string (time zone: KST)
 	attendance: z.array(z.string()),
 })
