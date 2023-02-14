e0616b57cf8cf18ed9de77fb3fde1d183cd00499
developomp <developomp@gmail.com>
2023-02-14 12:43:37 +0900

    refactor: improve user schema

--- src/types/user.ts
+++ src/types/user.ts
@@ -6,14 +6,14 @@ export const userSchema = z.object({
 	// user's tier based on their devRating
 	tier: tierSchema,
 
-	// total spendable points
-	points: z.number(),
+	// total spendable points. Integer. Can be negative.
+	points: z.number().int(),
 
 	// array of "YYYY/MM/DD" formatted string (time zone: KST)
-	attendance: z.array(z.string()),
+	attendance: z.array(z.string().regex(/\d\d\d\d[/]\d\d[/]\d\d/)),
 
 	// array of discord role IDs
-	roles: z.array(z.string()),
+	roles: z.array(z.string().regex(/\d+/)),
 })
 
 export type User = z.infer<typeof userSchema>
