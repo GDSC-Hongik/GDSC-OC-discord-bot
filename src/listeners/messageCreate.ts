28e42ec8745b8f9b6632df7f0fcb587847e47b4a
developomp <developomp@gmail.com>
2023-02-15 14:23:00 +0900

    refactor: prevent attendance update from running on system messages

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -10,7 +10,8 @@ export class MessageCreateListener extends Listener<
 	typeof Events.MessageCreate
 > {
 	public async run(message: Message) {
-		if (!message.author.bot)
+		// slash command 사용은 출석 처리 X
+		if (!message.author.bot && !message.author.system)
 			await devRatingEvent({
 				type: DevRatingEvent.UPDATE_ATTENDANCE,
 				data: [message.author.id, message.id],
