02d1cab04c3c259a0eca17dd72e70a580bb517a8
developomp <developomp@gmail.com>
2023-02-18 23:23:02 +0900

    feat: remove XP points on post delete

--- src/lib/devRating/postDelete.ts
+++ src/lib/devRating/postDelete.ts
@@ -1,7 +1,8 @@
 import type { ThreadChannel } from "discord.js"
 
+import { Activities } from "../../types/activities"
 import type { Post } from "../../types/post"
-import { deletePost, getUser, refs, setUser } from "../firebase"
+import { botCache, deletePost, getUser, refs, setUser } from "../firebase"
 
 export default async function (thread: ThreadChannel): Promise<void> {
 	const post = await refs.posts.where("discordLink", "==", thread.url).get()
@@ -9,10 +10,14 @@ export default async function (thread: ThreadChannel): Promise<void> {
 		const postID = postDoc.id
 		const postData = postDoc.data() as Post
 
-		// remove post from user
 		const user = await getUser(postData.author)
 		if (user) {
+			// remove post from post author
 			user.posts = user.posts.filter((userPostID) => userPostID !== postID)
+
+			// remove points from post author
+			user.points -= botCache.data.activityPoints[Activities.POST_CREATE]
+
 			setUser(postData.author, user)
 		}
 
