de07a27f4e50d8d9f2b661c57bbfbd4269014804
developomp <developomp@gmail.com>
2023-02-26 17:54:02 +0900

    refactor: sort user type properties alphabetically

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,27 +3,27 @@ import { z } from "zod"
 import { achievementsSchema } from "./achievements"
 
 export const userSchema = z.object({
-	// total spendable points. Integer. Can be negative.
-	points: z.number().int(),
+	achievements: z.array(achievementsSchema),
 
 	// array of "YYYY/MM/DD" formatted string (time zone: KST)
 	attendance: z.array(z.string().regex(/\d\d\d\d[/]\d\d[/]\d\d/)),
 
-	// array of discord role IDs
-	roles: z.array(z.string().regex(/\d+/)),
+	// total spendable points. Integer. Can be negative.
+	points: z.number().int(),
 
 	// array of post IDs
 	posts: z.array(z.string().regex(/[a-zA-Z0-9]+/)),
 
-	achievements: z.array(achievementsSchema),
+	// array of discord role IDs
+	roles: z.array(z.string().regex(/\d+/)),
 })
 
 export type User = z.infer<typeof userSchema>
 
 export const defaultUser: User = {
-	points: 0,
-	attendance: [],
-	roles: [],
-	posts: [],
 	achievements: [],
+	attendance: [],
+	points: 0,
+	posts: [],
+	roles: [],
 }
