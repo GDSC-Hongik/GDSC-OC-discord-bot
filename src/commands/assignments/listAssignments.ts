4aec81a7307affcc3092a050f0827fe02d544fd8
developomp <developomp@gmail.com>
2023-03-05 23:53:34 +0900

    feat(/과제-열람): show specific assignment info

--- src/commands/assignments/listAssignments.ts
+++ src/commands/assignments/listAssignments.ts
@@ -4,11 +4,17 @@ import { Command } from "@sapphire/framework"
 import { ChatInputCommandInteraction } from "discord.js"
 import { PermissionFlagsBits } from "discord.js"
 
-import { listAssignments } from "../../lib/firebase"
+import { getAssignment, listAssignments } from "../../lib/firebase"
 import { Assignment } from "../../types/assignments"
 
-const Options = {
-	showClosed: "마감된-과제-보이기",
+enum Options {
+	assignmentID = "과제-id",
+	showClosed = "마감된-과제-보이기",
+}
+
+interface Args {
+	assignmentID?: string
+	showClosed: boolean
 }
 
 export class ListAssignmentsCommand extends Command {
@@ -18,7 +24,10 @@ export class ListAssignmentsCommand extends Command {
 		registry.registerChatInputCommand((builder) =>
 			builder
 				.setName("과제-열람")
-				.setDescription("등록된 과제 나열")
+				.setDescription("등록된 과제 나열 / 특정 과제 열람")
+				.addStringOption((option) =>
+					option.setName(Options.assignmentID).setDescription("특정 과제 ID")
+				)
 				.addBooleanOption((option) =>
 					option
 						.setName(Options.showClosed)
@@ -30,9 +39,27 @@ export class ListAssignmentsCommand extends Command {
 	}
 
 	public async chatInputRun(interaction: ChatInputCommandInteraction) {
-		const showClosed = !!interaction.options.getBoolean(Options.showClosed)
+		const args = this.parseArgs(interaction)
 
-		await this.replySuccess(interaction, showClosed)
+		await this.replySuccess(interaction, args)
+	}
+
+	parseArgs(interaction: ChatInputCommandInteraction): Args {
+		return {
+			showClosed: !!interaction.options.getBoolean(Options.showClosed),
+			assignmentID:
+				interaction.options.getString(Options.assignmentID) ?? undefined,
+		}
+	}
+
+	formatAssignment(assignmentID: string, assignment: Assignment): string {
+		return `**이름** - ${assignment.name}
+**ID** - ${assignmentID}
+**GitHub 레포 이름** - ${assignment.repository}
+**파일 경로** - ${assignment.filePath}
+**인원** - ${assignment.members.length}명 (\`</과제-제출-확인>\`으로 확인)
+**마감** - ${assignment.closed}
+`
 	}
 
 	/**
@@ -47,13 +74,7 @@ export class ListAssignmentsCommand extends Command {
 		let index = 0
 		for (const assignmentID in assignments) {
 			const assignment = assignments[assignmentID]
-
-			const str = `**이름** - ${assignment.name}
-**ID** - ${assignmentID}
-**GitHub 레포 이름** - ${assignment.repository}
-**파일 경로** - ${assignment.filePath}
-**인원** - ${assignment.members.length}명 (\`</과제-제출-확인>\`으로 확인)
-**마감** - ${assignment.closed}
+			const str = `${this.formatAssignment(assignmentID, assignment)}
 
 `
 
@@ -68,11 +89,32 @@ export class ListAssignmentsCommand extends Command {
 		return result
 	}
 
-	async replySuccess(
+	async replyAssignmentInfo(
 		interaction: ChatInputCommandInteraction,
-		showClosed: boolean
+		args: Args
 	): Promise<void> {
-		const assignments = await listAssignments(showClosed)
+		if (!args.assignmentID)
+			return this.replyFail(interaction, "유효하지 않은 과제 ID입니다.")
+
+		const assignment = await getAssignment(args.assignmentID)
+		if (!assignment)
+			return this.replyFail(interaction, "유효하지 않은 과제 ID입니다.")
+
+		await interaction.reply({
+			embeds: [
+				new EmbedBuilder({
+					title: "과제 정보",
+					description: this.formatAssignment(args.assignmentID, assignment),
+				}),
+			],
+		})
+	}
+
+	async replyAssignmentList(
+		interaction: ChatInputCommandInteraction,
+		args: Args
+	): Promise<void> {
+		const assignments = await listAssignments(args.showClosed)
 		const assignmentDescriptions = this.formatAssignments(assignments)
 
 		await interaction.reply("등록된 과제를 나열합니다")
@@ -88,5 +130,32 @@ export class ListAssignmentsCommand extends Command {
 					}),
 				],
 			})
+	}
+
+	async replyFail(
+		interaction: ChatInputCommandInteraction,
+		reason: string
+	): Promise<void> {
+		await interaction.reply({
+			embeds: [
+				new EmbedBuilder({
+					title: "과제 정보 열람 실패!",
+					description: reason,
+				}),
+			],
+			ephemeral: true,
+		})
+	}
+
+	async replySuccess(
+		interaction: ChatInputCommandInteraction,
+		args: Args
+	): Promise<void> {
+		if (args.assignmentID) {
+			this.replyAssignmentInfo(interaction, args)
+			return
+		}
+
+		this.replyAssignmentList(interaction, args)
 	}
 }
